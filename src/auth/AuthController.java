7bea707551616b62601a4a8d09a5b523ff0b5d16
Arthur Desk <apploidyakutsk@gmail.com>
2019-02-26 00:07:17 +0300

    updated auth

--- src/auth/AuthController.java
+++ src/auth/AuthController.java
@@ -1,7 +1,35 @@
 package auth;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 /**
  * @author Arthur Kupriyanov
  */
 public class AuthController {
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button regusterBtn;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    void register(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login(ActionEvent event) {
+        System.out.println("Login " + loginField.textProperty().getValue());
+        System.out.println("Password " + passwordField.textProperty().getValue());
+    }
 }
